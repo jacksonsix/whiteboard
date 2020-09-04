8304dec8c6d3106e20322bcfda9cb62b5e00b4a8
whiteboard <jacksonsixmailbox@gmail.com>
2020-09-04 10:33:04 -0400

    http serve all folders

--- httpserver.js
+++ httpserver.js
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const requestListener = function (req, res) {
   
+var codepath='';
+
   //res.write('Hello World!');
   console.log(req.url);
   if(req.url =='/favicon.ico' || req.url.indexOf('timeout') > 0){
@@ -9,7 +11,7 @@ const requestListener = function (req, res) {
   }
   else  if(req.url.indexOf('.js') > 0){
 	 res.writeHead(200, {'Content-Type': 'text/javascript'});
-	 var filename ='.'+ req.url;
+	 var filename ='.'+ codepath + req.url;
 	 fs.readFile(filename, function(err, data) {
 		if (err) {
 			   res.write('read file error');  
@@ -20,22 +22,24 @@ const requestListener = function (req, res) {
 		res.end();
        });
   } else if(req.url.indexOf('.css') > 0){
-	 res.writeHead(200, {'Content-Type': 'text/css'});
-	 var filename ='.'+ req.url;
-	 fs.readFile(filename, function(err, data) {
-		if (err) {
-			   res.write('read file error');  
-			   res.end();
-			   throw err; 
-		}
-		res.write(data);  
-		res.end();
+	res.writeHead(200, {'Content-Type': 'text/css'});
+	var filename ='.'+ codepath +req.url;
+	fs.readFile(filename, function(err, data) {
+	if (err) {
+		   res.write('read file error');  
+		   res.end();
+		   throw err; 
+	}
+	res.write(data);  
+	res.end();
        });
   }else {
-	 res.writeHead(200, {'Content-Type': 'text/html'});
-	 var filename ='.'+ req.url;
-	 fs.readFile('dsdemo.html', function(err, data) {
-	//console.log(data);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var interm = req.url.lastIndexOf('/');
+	codepath = req.url.substring(0,interm);
+	var filename = '.' + req.url;
+	fs.readFile(filename, function(err, data) {
+
 	if (err) {
 		res.write('read file error');  
 	       res.end();

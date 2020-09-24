e021bbeb8c03a5a349b7164929187c69d0a54eda
whiteboard <jacksonsixmailbox@gmail.com>
2020-09-23 20:19:34 -0400

    httpserver fix

--- httpserver.js
+++ httpserver.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 //const fileserver = require('./fileserver/node_modules/formidable');
+var d = require('domain').create()
+d.on('error', function(err){
+    // handle the error safely
+    console.log(err)
+})
 
 const requestListener = function (req, res) {
   
@@ -9,19 +14,22 @@ const requestListener = function (req, res) {
   //res.write('Hello World!');
   console.log(req.url);
   if(req.url =='/favicon.ico' || req.url.indexOf('timeout') > 0){
-    //res.writeHead(200, {'Content-Type': 'text/javascript'});  
+    res.writeHead(200, {'Content-Type': 'text/javascript'});  
+    res.end();
   }
   else  if(req.url.indexOf('.js') > 0){
 	 res.writeHead(200, {'Content-Type': 'text/javascript'});
 	 var filename ='.'+ codepath + req.url;
 	 fs.readFile(filename, function(err, data) {
 		if (err) {
-			   res.write('read file error');  
+			   console.log( err);  
 			   res.end();
-			   throw err; 
-		}
+			    
+		}else{
 		res.write(data);  
 		res.end();
+		}
+
        });
   } else if(req.url.indexOf('.css') > 0){
 	res.writeHead(200, {'Content-Type': 'text/css'});
@@ -30,26 +38,41 @@ const requestListener = function (req, res) {
 	if (err) {
 		   res.write('read file error');  
 		   res.end();
-		   throw err; 
+		   console.log( err); 
 	}
 	res.write(data);  
 	res.end();
-       });
+       });  
+  }else if(req.url.indexOf('.svg') > 0  || req.url.indexOf('.img') > 0 ||  req.url.indexOf('.png') > 0){
+	res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+	var filename ='.'+ codepath +req.url;
+	fs.readFile(filename, function(err, data) {
+	if (err) {
+		   res.write('read file error');  
+		   res.end();
+		    console.log( err); 
+	}
+	res.write(data);  
+	res.end();
+       });  
   }else {
-	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.writeHead(200, {'Content-Type': 'text/html',
+           'Set-Cookie':["SameSite=None"]
+         });
+        
 	var interm = req.url.lastIndexOf('/');
 	codepath = req.url.substring(0,interm);
 	var filename = '.' + req.url;
 	fs.readFile(filename, function(err, data) {
-
-	if (err) {
-		res.write('read file error');  
-	       res.end();
-	       throw err; 
-	}
-	res.write(data);  
-	res.end();
-  });
+                if(err){ 
+                  console.log(err);
+                }else{
+		res.write(data);  
+		res.end();
+                }
+	
+	 });
+	
   }
 
   
